201756747ca0137dd52f015e9483e742995e51b6
bondarika <bondarika@yandex.ru>
2025-07-16 13:06:29 +0300

    переезд с render на vercel

--- src/shared/api/apolloClient.ts
+++ src/shared/api/apolloClient.ts
@@ -2,6 +2,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
-  uri: 'https://t1-task.onrender.com',
+  uri: 'https://t1-task-227a.vercel.app/api/graphql',
   cache: new InMemoryCache(),
 });
