7fa818c73e681d12231059503a122427bdddbb6f
bondarika <bondarika@yandex.ru>
2025-07-20 06:23:26 +0300

    на всякий пути прописаны

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
   build: {
     outDir: 'dist',
   },
