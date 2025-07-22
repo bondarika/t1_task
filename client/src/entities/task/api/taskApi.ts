cd5579e86b257cfe415d8be22b229b5cd733c16b
bondarika <bondarika@yandex.ru>
2025-07-22 16:08:03 +0300

    fix

--- client/src/entities/task/api/taskApi.ts
+++ client/src/entities/task/api/taskApi.ts
@@ -1,5 +1,5 @@
 ﻿import { type Task, type CreateTaskData, type UpdateTaskData } from '../model/types.ts';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5173';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 
 /**
  * Кастомный класс ошибки для API запросов
